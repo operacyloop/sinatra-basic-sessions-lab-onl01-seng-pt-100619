06ffef199619f6eb6f6d6b525bb9f4958938784a
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-02-10 21:34:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,47 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
+configure do
+  enable :sessions
+  set :session_secret, "secret"
 end
+
+class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
+end
+
+
+# require_relative 'config/environment'
+
+# class App < Sinatra::Base
+  
+#   configure do
+#     enable :sessions
+#     set :session_secret, "secret"
+#   end
+
+#   get '/' do
+#     erb :index
+#   end
+  
+#   post 'checkout/' do
+# #     #@analyzed_text = TextAnalyzer.new(params[:user_text])
+#     @item = params[:item]
+#     @price = params[:price]
+     
+#     session["item"] = @item
+#     @sesson = session
+    
+#     erb :checkout
+#   end
+
+# # end
+# end
